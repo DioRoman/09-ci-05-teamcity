5e9c23599eb1c215650239cfeb1aed532172448e
DioRoman <roman-ronin@yandex.ru>
2025-08-10 22:56:06 +0300

    0.1.0

--- terraform/main.tf
+++ terraform/main.tf
@@ -55,60 +55,60 @@ module "yandex-vpc" {
 
 # Создание VM
 
-module "teamcity-server" {
-  source              = "git::https://github.com/DioRoman/ter-final.git//src/modules/yandex-vm?ref=main"
-  vm_name             = var.teamcity-server[0].instance_name 
-  vm_count            = var.teamcity-server[0].instance_count
-  zone                = var.vpc_default_zone[0]
-  subnet_ids          = module.yandex-vpc.subnet_ids
-  image_id            = data.yandex_compute_image.ubuntu.id
-  platform_id         = var.teamcity-server[0].platform_id
-  cores               = var.teamcity-server[0].cores
-  memory              = var.teamcity-server[0].memory
-  disk_size           = var.teamcity-server[0].disk_size 
-  public_ip           = var.teamcity-server[0].public_ip
-  security_group_ids  = [module.yandex-vpc.security_group_ids["web"]]
+# module "teamcity-server" {
+#   source              = "git::https://github.com/DioRoman/ter-final.git//src/modules/yandex-vm?ref=main"
+#   vm_name             = var.teamcity-server[0].instance_name 
+#   vm_count            = var.teamcity-server[0].instance_count
+#   zone                = var.vpc_default_zone[0]
+#   subnet_ids          = module.yandex-vpc.subnet_ids
+#   image_id            = data.yandex_compute_image.ubuntu.id
+#   platform_id         = var.teamcity-server[0].platform_id
+#   cores               = var.teamcity-server[0].cores
+#   memory              = var.teamcity-server[0].memory
+#   disk_size           = var.teamcity-server[0].disk_size 
+#   public_ip           = var.teamcity-server[0].public_ip
+#   security_group_ids  = [module.yandex-vpc.security_group_ids["web"]]
   
-  labels = {
-    env  = var.teamcity-server[0].env_name
-    role = var.teamcity-server[0].role
-  }
+#   labels = {
+#     env  = var.teamcity-server[0].env_name
+#     role = var.teamcity-server[0].role
+#   }
 
-  metadata = {
-    user-data = data.template_file.teamcity-server.rendered
-    serial-port-enable = local.serial-port-enable
-  }  
-}
+#   metadata = {
+#     user-data = data.template_file.teamcity-server.rendered
+#     serial-port-enable = local.serial-port-enable
+#   }  
+# }
 
-module "teamcity-agent" {
-  source              = "git::https://github.com/DioRoman/ter-final.git//src/modules/yandex-vm?ref=main"
-  vm_name             = var.teamcity-agent[0].instance_name 
-  vm_count            = var.teamcity-agent[0].instance_count
-  zone                = var.vpc_default_zone[0]
-  subnet_ids          = module.yandex-vpc.subnet_ids
-  image_id            = data.yandex_compute_image.ubuntu.id
-  platform_id         = var.teamcity-agent[0].platform_id
-  cores               = var.teamcity-agent[0].cores
-  memory              = var.teamcity-agent[0].memory
-  disk_size           = var.teamcity-agent[0].disk_size 
-  public_ip           = var.teamcity-agent[0].public_ip
-  security_group_ids  = [module.yandex-vpc.security_group_ids["web"]]
+# module "teamcity-agent" {
+#   source              = "git::https://github.com/DioRoman/ter-final.git//src/modules/yandex-vm?ref=main"
+#   vm_name             = var.teamcity-agent[0].instance_name 
+#   vm_count            = var.teamcity-agent[0].instance_count
+#   zone                = var.vpc_default_zone[0]
+#   subnet_ids          = module.yandex-vpc.subnet_ids
+#   image_id            = data.yandex_compute_image.ubuntu.id
+#   platform_id         = var.teamcity-agent[0].platform_id
+#   cores               = var.teamcity-agent[0].cores
+#   memory              = var.teamcity-agent[0].memory
+#   disk_size           = var.teamcity-agent[0].disk_size 
+#   public_ip           = var.teamcity-agent[0].public_ip
+#   security_group_ids  = [module.yandex-vpc.security_group_ids["web"]]
   
-  labels = {
-    env  = var.teamcity-agent[0].env_name
-    role = var.teamcity-agent[0].role
-  }
+#   labels = {
+#     env  = var.teamcity-agent[0].env_name
+#     role = var.teamcity-agent[0].role
+#   }
 
-  depends_on = [
-  module.teamcity-server
-  ]
+#   depends_on = [
+#   module.teamcity-server
+#   ]
 
-  metadata = {
-    user-data = data.template_file.teamcity-agent.rendered
-    serial-port-enable = local.serial-port-enable
-    server-url = "http://${module.teamcity-server.external_ips[0]}:8111"
-  } 
-}
+#   metadata = {
+#     user-data = data.template_file.teamcity-agent.rendered
+#     serial-port-enable = local.serial-port-enable
+#     server-url = "http://${module.teamcity-server.external_ips[0]}:8111"
+#   } 
+# }
 
 module "nexus-vm" {
   source              = "git::https://github.com/DioRoman/ter-final.git//src/modules/yandex-vm?ref=main"
@@ -129,9 +129,9 @@ module "nexus-vm" {
     role = var.nexus-vm[0].role
   }
 
-  depends_on = [
-  module.teamcity-agent
-  ]
+  # depends_on = [
+  # module.teamcity-agent
+  # ]
 
   metadata = {
     user-data = data.template_file.nexus-vm.rendered
@@ -140,20 +140,20 @@ module "nexus-vm" {
 }
 
 # Инициализация 
-data "template_file" "teamcity-server" {
-  template = file("./teamcity-server-init.yml")
-    vars = {
-    ssh_public_key     = file(var.vm_ssh_root_key)
-  }
-}
+# data "template_file" "teamcity-server" {
+#   template = file("./teamcity-server-init.yml")
+#     vars = {
+#     ssh_public_key     = file(var.vm_ssh_root_key)
+#   }
+# }
 
-data "template_file" "teamcity-agent" {
-  template = file("./teamcity-agent-init.yml")
-    vars = {
-    ssh_public_key     = file(var.vm_ssh_root_key)
-    server-url = "http://${module.teamcity-server.external_ips[0]}:8111"
-  }
-}
+# data "template_file" "teamcity-agent" {
+#   template = file("./teamcity-agent-init.yml")
+#     vars = {
+#     ssh_public_key     = file(var.vm_ssh_root_key)
+#     server-url = "http://${module.teamcity-server.external_ips[0]}:8111"
+#   }
+# }
 
 data "template_file" "nexus-vm" {
   template = file("./nexus-vm-init.yml")
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,27 +1,27 @@
 # Web outputs
-output "teamcity-agent_private_ips" {
-  description = "Private IP addresses of Web VMs"
-  value       = module.teamcity-agent.internal_ips
-}
+# output "teamcity-agent_private_ips" {
+#   description = "Private IP addresses of Web VMs"
+#   value       = module.teamcity-agent.internal_ips
+# }
 
-output "teamcity-agent_ssh" {
-  description = "SSH commands to connect to Web VMs"
-  value = [
-    for ip in module.teamcity-agent.external_ips : "ssh -l ubuntu ${ip}"
-  ]
-}
+# output "teamcity-agent_ssh" {
+#   description = "SSH commands to connect to Web VMs"
+#   value = [
+#     for ip in module.teamcity-agent.external_ips : "ssh -l ubuntu ${ip}"
+#   ]
+# }
 
-output "teamcity-server_private_ips" {
-  description = "Private IP addresses of Web VMs"
-  value       = module.teamcity-server.internal_ips
-}
+# output "teamcity-server_private_ips" {
+#   description = "Private IP addresses of Web VMs"
+#   value       = module.teamcity-server.internal_ips
+# }
 
-output "teamcity-server_ssh" {
-  description = "SSH commands to connect to Web VMs"
-  value = [
-    for ip in module.teamcity-server.external_ips : "ssh -l ubuntu ${ip}"
-  ]
-}
+# output "teamcity-server_ssh" {
+#   description = "SSH commands to connect to Web VMs"
+#   value = [
+#     for ip in module.teamcity-server.external_ips : "ssh -l ubuntu ${ip}"
+#   ]
+# }
 
 output "nexus-vm_private_ips" {
   description = "Private IP addresses of Web VMs"
@@ -35,6 +35,6 @@ output "nexus-vm_ssh" {
   ]
 }
 
-output "teamcity-server-external_ip" {
-  value = module.teamcity-server.external_ips
-}
+# output "teamcity-server-external_ip" {
+#   value = module.teamcity-server.external_ips
+# }
